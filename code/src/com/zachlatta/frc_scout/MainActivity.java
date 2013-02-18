9ad2c9943a99fa53b191953d9e3806a47fa3f064
Zachary Latta <zchlatta@gmail.com>
2013-02-18 02:24:56 -0800

    Closes #7

--- code/src/com/zachlatta/frc_scout/MainActivity.java
+++ code/src/com/zachlatta/frc_scout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zachlatta.frc_scout;
 
 import android.app.ListActivity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity
 {
@@ -18,6 +20,7 @@ public class MainActivity extends ListActivity
     private static final int ACTIVITY_EDIT = 1;
 
     private static final int INSERT_ID = Menu.FIRST;
+    private static final int FEATURE_REQUEST_ID = Menu.FIRST + 1;
     private static final int DELETE_ID = Menu.FIRST + 1;
 
     private NotesDbAdapter mDbHelper;
@@ -41,6 +44,7 @@ public class MainActivity extends ListActivity
     {
         boolean result = super.onCreateOptionsMenu(menu);
         menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        menu.add(0, FEATURE_REQUEST_ID, 1, R.string.menu_feature_request);
         return result;
     }
 
@@ -51,6 +55,14 @@ public class MainActivity extends ListActivity
         {
             case INSERT_ID:
                 createNote();
+                return true;
+
+            case FEATURE_REQUEST_ID:
+                sendEmail(
+                        new String[] {"zchlatta@gmail.com"},
+                        "Feature Request - FRC Scout",
+                        "Brief summary:\n\n\n" +
+                                "Extended description (optional): \n");
                 return true;
         }
 
@@ -117,7 +129,7 @@ public class MainActivity extends ListActivity
         String[] from = new String[]{NotesDbAdapter.KEY_NAME};
 
         // Array of the fields to bind fields displayed in the list to.
-        int[] to = new int[]{R.id.text1};
+        int[] to = new int[] {R.id.text1};
 
         SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.notes_row, notesCursor, from, to);
         setListAdapter(notes);
@@ -127,5 +139,23 @@ public class MainActivity extends ListActivity
     {
         Intent intent = new Intent(this, NoteEdit.class);
         startActivityForResult(intent, ACTIVITY_CREATE);
+    }
+
+    private void sendEmail(String[] recipients, String subject, String body)
+    {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, recipients);
+        i.putExtra(Intent.EXTRA_SUBJECT, subject);
+        i.putExtra(Intent.EXTRA_TEXT, body);
+
+        try
+        {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        }
+        catch (ActivityNotFoundException e)
+        {
+            Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
